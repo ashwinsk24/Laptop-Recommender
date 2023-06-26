a3dd27175b876a0dd8fcb5856680fea45348dc04
Ashwin Sivasankaran <ashwin.kolapadath@gmail.com>
2023-06-26 19:17:22 +0530

    major update

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
 from flask import Flask, request, render_template
 import pickle
-import requests
-import pandas as pd
-from patsy import dmatrices
 
 laptop = pickle.load(open('model/usecases_list.pkl', 'rb'))
 similarity = pickle.load(open('model/similarity.pkl', 'rb'))
 
 
-def recommendmovq(use):
-    index = laptop[laptop['usecases'] == use].index[0]
-    distances = sorted(
-        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    printed_names = set()
-    recommended_laptop_name = []
-    recommended_laptop_image = []
-    for i in distances[1:500]:
-        laptop_id = laptop.iloc[i[0]].laptop_id
-        recommended_laptop_name.append(laptop.iloc[i[0]].name)
-
-    return recommended_laptop_name
-
 def recommendmov(use):
     index = laptop[laptop['usecases'] == use].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_laptop_name = []
+    recommended_laptops = []
     printed_names = set()
-    for i in distances[1:6]:
+    for i in distances[1:10]:
         row_index = i[0]
         name = laptop.iloc[row_index]['name']
+        img_link = laptop.iloc[row_index]['img_link']
+        price = laptop.iloc[row_index]['price']
         if name not in printed_names:
             printed_names.add(name)
-            recommended_laptop_name.append(name)
-    return recommended_laptop_name
+            recommended_laptops.append({'name': name, 'img_link': img_link, 'price': price})
+    return recommended_laptops
+
 
 app = Flask(__name__)
 
@@ -51,29 +38,56 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/product", methods=['GET', 'POST'])
+def product():
+    usecase_list = laptop['usecases'].unique()
+    status = False
 
-@app.route("/recommend", methods=['GET', 'POST'])
-def recommend():
-
-    usecase_list = laptop['usecases'].values
-    status=False
     if request.method == "POST":
         try:
             if request.form:
                 usecase = request.form['usecases']
-                print(usecase)
-                recommended_laptop_name = recommendmov(usecase)
-                print(recommended_laptop_name)
+                recommended_laptops = recommendmov(usecase)
+                laptop_names = [laptop['name'] for laptop in recommended_laptops]
+                img_links = [laptop['img_link'] for laptop in recommended_laptops]
+                prices = [laptop['price'] for laptop in recommended_laptops]
                 status = True
-                
-                return render_template("prediction.html", laptop_name=recommended_laptop_name, usecase_list=usecase_list, status = status)
+
+                return render_template("product.html", laptop_names=laptop_names, img_links=img_links, prices=prices, usecase_list=usecase_list, status=status)
 
         except Exception as e:
             error = {'error': e}
-            return render_template("prediction.html",error = error, usecase_list=usecase_list, status = status)
+            return render_template("product.html", error=error, usecase_list=usecase_list, status=status)
 
     else:
-        return render_template("prediction.html", usecase_list=usecase_list, status = status)
+        return render_template("product.html", usecase_list=usecase_list, status=status)
+
+
+@app.route("/recommend", methods=['GET', 'POST'])
+def recommend():
+
+    usecase_list = laptop['usecases'].unique()
+    status = False
+
+    if request.method == "POST":
+        try:
+            if request.form:
+                usecase = request.form['usecases']
+                recommended_laptops = recommendmov(usecase)
+                laptop_names = [laptop['name'] for laptop in recommended_laptops]
+                img_links = [laptop['img_link'] for laptop in recommended_laptops]
+                prices = [laptop['price'] for laptop in recommended_laptops]
+                status = True
+
+                return render_template("prediction.html", laptop_names=laptop_names, img_links=img_links, usecase_list=usecase_list, status=status)
+
+        except Exception as e:
+            error = {'error': e}
+            return render_template("prediction.html", error=error, usecase_list=usecase_list, status=status)
+
+    else:
+        return render_template("prediction.html", usecase_list=usecase_list, status=status)
+
 
 
 if __name__ == '__main__':
